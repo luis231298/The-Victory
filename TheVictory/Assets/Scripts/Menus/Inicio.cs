747504f87fa76c1ff7babf7e7dcf25cfdd30c82c
José Luis Luna <pepeluna23@yahoo.com>
2022-09-12 12:37:45 -0500

    Update Inicio.css
    Ya permite terminar la ejecución en el menu principal

--- TheVictory/Assets/Scripts/Menus/Inicio.cs
+++ TheVictory/Assets/Scripts/Menus/Inicio.cs
@@ -33,6 +33,10 @@ public class Inicio : MonoBehaviour
 
     public void Exit()
     {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
         Application.Quit();
+    #endif
     }
 }
